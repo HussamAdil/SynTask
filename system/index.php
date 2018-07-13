e9df244f50ec7b12407c34708c443bf3d50d2d57
Hussam Adil <hussam0683@gmail.com>
2018-07-13 20:02:49 +0300

    change bg colors and add main color fix narbar

--- system/index.php
+++ system/index.php
@@ -6,8 +6,16 @@ include("navbar.php");
  
 ?>
 
-<p class="container">Hello world</p>	
+<div class="container"> 
+<h3>Hello</h3>
+<div class="" style="background: #FBF8FF">
 
+<p>
+	Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. 
+</p>
+
+
+</div>	
 <?php 
 
 include("footer.php");
