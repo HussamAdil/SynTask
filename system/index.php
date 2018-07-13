2c270c193d3b90e92e50a21257d4c9f465411875
Hussam Adil <hussam0683@gmail.com>
2018-07-13 22:00:53 +0300

    add new navbar and changed color and add container

--- system/index.php
+++ system/index.php
@@ -7,15 +7,29 @@ include("navbar.php");
 ?>
 
 <div class="container"> 
-<h3>Hello</h3>
-<div class="" style="background: #FBF8FF">
+		<div class="col-sm-6">
+			<h3><i class="fa fa-tag"></i> News  </h3>
+			<div class="News">
 
-<p>
-	Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. 
-</p>
+			<p>
+				Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. 
+			</p>
 
+			</div>
 
-</div>	
+	</div>
+			<div class="col-sm-6">
+			<h3><i class="fa fa-tag"></i> News  </h3>
+			<div class="News">
+
+			<p>
+				Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. 
+			</p>
+
+			</div>
+
+	</div>	
+</div>
 <?php 
 
 include("footer.php");
--- system/navbar.php
+++ system/navbar.php
@@ -3,6 +3,7 @@
 <head>
 	<title></title>
 	<link rel="stylesheet" type="text/css" href="css/app.css">
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
 </head>
 <body>
 <nav class="navbar navbar-default">
