668f9732bd237c9a6c81f49ea1382eb687eebcfa
Hussam Adil <hussam0683@gmail.com>
2018-07-13 22:50:03 +0300

    change color and fix error

--- system/index.php
+++ system/index.php
@@ -10,7 +10,7 @@ include("navbar.php");
 		<div class="col-sm-6">	
 			<div class="panel panel-default">
 		  <!-- Default panel contents -->
-		  <div class="panel-heading">
+		  <div class="panel-heading" style="background: #211869;color: #fff;">
 		  	<h3><i class="fa fa-tag"></i> News  </h3>
 		  </div>
 		  <!-- List group -->
@@ -20,14 +20,16 @@ include("navbar.php");
 		   <a href="#"> <li class="list-group-item">Cras justo odio</li></a>
 
 		   <a href="#"> <li class="list-group-item">Cras justo odio</li></a>
-
+		</ul>
 		</div>
 	</div>
+
+
 			<div class="col-sm-6">	
 			<div class="panel panel-default">
 		  <!-- Default panel contents -->
-		  <div class="panel-heading">
-		  	<h3><i class="fa fa-tag"></i> News  </h3>
+		  <div class="panel-heading" style="background: #554776;color: #fff;">
+		  	<h3><i class="fa fa-fire"></i> Annoucement  </h3>
 		  </div>
 		  <!-- List group -->
 		  <ul class="list-group">
@@ -36,9 +38,49 @@ include("navbar.php");
 		   <a href="#"> <li class="list-group-item">Cras justo odio</li></a>
 
 		   <a href="#"> <li class="list-group-item">Cras justo odio</li></a>
-
+		</ul>
 		</div>
 	</div>
+
+
+			<div class="col-sm-6">	
+			<div class="panel panel-default" >
+		  <!-- Default panel contents -->
+		  <div class="panel-heading" style="background: #9A7CF8;color: #fff;">
+		  	<h3><i class="fa fa-fire"></i> Annoucement  </h3>
+		  </div>
+		  <!-- List group -->
+		  <ul class="list-group">
+		   <a href="#"> <li class="list-group-item">Cras justo odio</li></a>
+
+		   <a href="#"> <li class="list-group-item">Cras justo odio</li></a>
+
+		   <a href="#"> <li class="list-group-item">Cras justo odio</li></a>
+		</ul>
+		</div>
+	</div>
+
+
+
+
+			<div class="col-sm-6">	
+			<div class="panel panel-default">
+		  <!-- Default panel contents -->	
+		  <div class="panel-heading"  style="background: #4C3A91;color: #fff;">
+		  	<h3><i class="fa fa-fire"></i> Annoucement  </h3>
+		  </div>
+		  <!-- List group -->
+		  <ul class="list-group">
+		   <a href="#"> <li class="list-group-item">Cras justo odio</li></a>
+
+		   <a href="#"> <li class="list-group-item">Cras justo odio</li></a>
+
+		   <a href="#"> <li class="list-group-item">Cras justo odio</li></a>
+		</ul>
+		</div>
+	</div>
+
+
 </div>
 <?php 
 
--- system/navbar.php
+++ system/navbar.php
@@ -23,14 +23,17 @@
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
         <li class="active"><a href="#">Home <span class="sr-only">(current)</span></a></li>
-        <li><a href="#">Link</a></li>
-             <li><a href="#">Lesson</a></li>
+              <li><a href="#">Lesson</a></li>
          <li><a href="#">duties</a></li>
         <li><a href="#">annons</a></li>
  
       </ul>
       <ul class="nav navbar-nav navbar-right">
-
+        <li>
+         <p> <i style="margin-top: 8px;
+         font-size: 23px;
+"      class="fa fa-bell-o" ></i></p>
+        </li>
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
           <ul class="dropdown-menu">
