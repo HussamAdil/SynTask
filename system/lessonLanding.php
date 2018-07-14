3560a744e53fbf7eb3f85641c11ffac0a4dc2102
Hussam Adil <hussam0683@gmail.com>
2018-07-14 13:14:21 +0300

    landong page

---
+++ system/lessonLanding.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/app.css">
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<style>
+	body 
+	{
+		background: #fff
+	}
+</style>
+<body >
+<nav class="navbar navbar-default">
+  <div class="container">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Project</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home <span class="sr-only">(current)</span></a></li>
+              <li><a href="#">Lesson</a></li>
+         <li><a href="#">duties</a></li>
+        <li><a href="#">annons</a></li>
+ 
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+<div class="mobile-session">
+	<div class="container">
+		<div class="col-sm-4">
+
+			<h1 style="text-transform:uppercase;margin-top: 50px">Compatible design</h1>
+		<span class="mobile-circle"></span>
+		<span class="mobile-circle"></span>
+		<span class="mobile-circle"></span>
+			<h3>
+				Our Web application mobile friendly design and portable
+			</h3>
+			<a href="" >Login </a>
+ 		</div>
+		<div class="col-sm-3 text-center">
+		<img src="img/mobile.png" width="450px" height="350" >
+	</div>
+	</div>
+	</div>
+	<div class="stat text-center">
+		<div class="container">
+		<h3>Numbers And Fact </h3>
+		<div class="col-sm-4 sub-stat">
+			<div class="icon-stata">
+				<i class="fa fa-download "></i>
+			</div>
+			<p>1 million download </p>
+		</div>
+
+		<div class="col-sm-4 sub-stat">
+			<div class="icon-stata">
+				<i class="fa fa-bolt "></i>
+			</div>
+			<p> 300,000 Daily Active Users </p>
+		</div>
+				<div class="col-sm-4 sub-stat">
+			<div class="icon-stata">
+				<i class="fa fa-check "></i>
+			</div>
+			<p>900,00 Installed</p>
+		</div>
+	</div>
+	</div>
+<div class="header-landing">
+	<div class="container">
+		<div class="text col-sm-4 col-sm-offset-1">
+			<h1>Learn More</h1>
+		<h3>Smart | Easy </h3>
+		<p style="    border-bottom: 1px solid #271bc1;
+    padding: 10px;
+    border-radius: 20px;
+">
+			Project is a place to gain and share knowledge. It's a platform to ask questions and connect with people who contribute unique insights and quality answers.
+		</p>
+		<a href="#">Start Learn Now !</a>
+	</div>
+	<div class="col-sm-6 ">
+	<img width="550px" height="450px"  src="img/team.png">
+</div>
+</div>
+</div>
+<div class="connect">
+	<div class="container">
+		<div class="">
+			<p>Connect </p>
+		</div>
+	</div>
+	</div>
+
+<?php 
+
+include("footer.php");
+
+?>
