695962f75156342b364ad433dec044fadc0a20ef
Mobarmajaty <abdelmajedyassin@gmail.com>
2018-07-14 03:46:27 +0300

    update My Duties, Lesson.php and sytle.css v.3

--- system/footer.php
+++ system/footer.php
@@ -1,9 +1,6 @@
 
 <script type="text/javascript" src="js/app.js"></script>
-<<<<<<< HEAD
 
-=======
 <script type="text/javascript" src="js/script.js"></script>
->>>>>>> 91ecc7c759d3fdb01c348d7e797465bb85f52cde
 </body>
 </html>
